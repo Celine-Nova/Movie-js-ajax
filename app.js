a2bfe151d5e72f82a3fcba6d49eac1e302759ba5
unknown <cocotte243@gmail.com>
2020-10-15 16:47:10 +0200

    second commit cours finish

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var app = {
+    
     init: function(){
         app.getMovie();
      
-        let valueSearch = document.getElementById('search');
+        let inputSearch = document.getElementById('search');
         // au touche du clavier sur le input je rveux récuperer sa valeur
-        valueSearch.addEventListener('keypress',app.getDataForm);
+        let test = inputSearch.addEventListener('keypress',app.getDataForm);
+        console.log('test : '+ app.test);
         let submit = document.getElementById('submit');
         //au click du bouton je veux comparer la velaur du form à la valeur d'un titre
         submit.addEventListener('click', app.valueFormSearch);
-        
+        value= app.getDataForm();
+        console.log("la valeur "  +value);
     },
-
+    
     getMovie: function(){
         fetch('https://api.themoviedb.org/3/movie/top_rated?sort_by=popularity.desc&language=fr-FR&api_key=87dfa1c669eea853da609d4968d294be')
         .then(data => data.json())
@@ -39,27 +42,30 @@ var app = {
             }
         })
     },
-    getDataForm: function(){
-        //je récupère la valeur du formulaire
-        let valueSearch = document.getElementById('search');
-        return valueSearch.value;
+        getDataForm: function(){
+        //je récupère le input
+        let input = document.getElementById('search');
+        let valueSearch = input.value
+        // console.log(valueSearch.value);
+
+        return valueSearch;
     },
     //je veux comparer la valeur du form à la valeur d'un titre
     valueFormSearch: function(){
-        
-        fetch('https://api.themoviedb.org/3/movie/top_rated?sort_by=popularity.desc&language=fr-FR&api_key=87dfa1c669eea853da609d4968d294be')
+       
+        fetch('https://api.themoviedb.org/3/search/movie?api_key=87dfa1c669eea853da609d4968d294be&language=en-US&query=%27'+ query +'&page=1&include_adult=false')
         .then(data => data.json())
         .then(response =>{
-            for(movie of response.results){
-                let comparaison = app.getDataForm();
-                console.log('coucou');
-                if(comparaison == movie.title ){
-                    console.log("le titre du film");
-                }else{
-                    console.log('ça ne marche pas');
-                }
+            console.log(response);
+            // for(movie of response.results){
+            //     let comparaison = app.getDataForm();
+            //     if(comparaison == movie.title ){
+            //         console.log("le titre du film");
+            //     }else{
+            //         console.log('ça ne marche pas');
+            //     }
             
-            }
+            //}
         })
     }
 }
